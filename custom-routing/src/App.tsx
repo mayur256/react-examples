45720c0718bc52b773db701ae1c39e7c904c675f
Mayur Upadhayay <mayur123a@gmail.com>
2022-07-14 22:57:03 +0530

    Initial Commit for custom-routing

---
+++ custom-routing/src/App.tsx
@@ -0,0 +1,15 @@
+
+// Atoms / Molecules/ Organisms
+import Navbar from "./components/organisms/Navbar";
+
+// Main Component definition
+function App() {
+  // Main JSX
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
