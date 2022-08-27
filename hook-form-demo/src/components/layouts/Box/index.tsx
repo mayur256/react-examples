3de80135a05a82c9969a532dd98bc9169db4cf34
Mayur Upadhayay <mayur123a@gmail.com>
2022-08-27 23:26:39 +0530

    feat: Added basic layout components

---
+++ hook-form-demo/src/components/layouts/Box/index.tsx
@@ -0,0 +1,19 @@
+// Top level imports
+import { ComponentPropsWithRef, ReactElement, ReactNode } from "react";
+
+// CSS module
+import classes from "./grid.module.css";
+
+// Props type definitions
+interface IProps extends ComponentPropsWithRef<"div"> {
+    children: ReactNode
+}
+
+// Component definition
+export default function Box({ children }: IProps): ReactElement {
+    return (
+        <div className={classes['display-grid']}>
+            {children}
+        </div>
+    )
+};
---
+++ hook-form-demo/src/components/layouts/Grid/index.tsx
@@ -0,0 +1,21 @@
+// Top level imports
+import { ComponentPropsWithRef, ReactElement, ReactNode } from "react";
+
+// CSS module
+import classes from "./grid.module.css";
+
+// Props type definitions
+interface IProps extends ComponentPropsWithRef<"div"> {
+    children: ReactNode
+}
+
+// Component definition
+export default function Grid({ children }: IProps): ReactElement {
+    return (
+        <div className={`
+            ${classes['display-grid']}
+        `}>
+            {children}
+        </div>
+    )    
+};
