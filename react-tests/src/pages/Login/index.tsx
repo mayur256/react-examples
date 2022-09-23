3bd383a53757d98047b11f9ced2dd048e2335bc4
Mayur Upadhayay <mayur123a@gmail.com>
2022-09-24 00:27:08 +0530

    feat: Integrated SASS and minimal login page

--- react-tests/src/pages/Login/index.tsx
+++ react-tests/src/pages/Login/index.tsx
@@ -1,12 +1,34 @@
 // Top level imports
-import { ReactElement } from "react";
+import { FormEvent, ReactElement } from "react";
+
+// import external scss files
+import "./index.scss";
 
 // Component definition
 export default function Login(): ReactElement {
+
+    /** Handler function - starts */
+    const handleFormSubmit = (event: FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+        console.log(event);
+    };
+    /** Handler function - ends */
+    // Main JSX
     return (
-        <form>
-            <label>User Name*</label>
-            <input type="text" required />
+        <form onSubmit={handleFormSubmit}>
+            <h1>Welcome</h1>
+            
+            <div className="rowField">
+                <input type="text" name="email" placeholder="Email" required />
+            </div>
+
+            <div className="rowField">
+                <input type="text" name="password" placeholder="Password" required />
+            </div>
+
+            <div className="rowField">
+                <button type="submit">Submit</button>
+            </div>
        </form>
     )
 }
