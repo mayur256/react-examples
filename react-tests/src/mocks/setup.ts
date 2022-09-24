24941d1f44abe9ace20e7b0a17af3eaefc0d5a2c
Mayur Upadhayay <mayur123a@gmail.com>
2022-09-24 23:27:53 +0530

    feat: Enabled MSW for testing

---
+++ react-tests/src/mocks/setup.ts
@@ -0,0 +1,9 @@
+import { setupWorker } from "msw";
+// import { setupServer } from "msw/node";
+import { handlers } from "./handlers";
+
+// worker to mock APIs in browser
+// export const worker = setupWorker(...handlers);
+
+// mock server to intercept API calls in test env
+export const server = setupWorker(...handlers);
