b3154a654cc5005afbc992526b1bdb330203b5d7
Mayur Upadhayay <mayur123a@gmail.com>
2022-09-28 00:12:58 +0530

    tests: Test case for navigation within the app

--- react-tests/src/pages/AppContainer/index.tsx
+++ react-tests/src/pages/AppContainer/index.tsx
@@ -2,17 +2,27 @@
 import { ReactElement } from "react";
 
 // React Router
-import { Outlet } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+
+// External CSS
+import "./index.scss";
 
 // Components
 import Navbar from "../../components/Navbar";
+import Home from "../Home";
+import Contact from "../Contact";
 
 // Component definition
 export default function AppContainer(): ReactElement {
     return (
         <>
             <Navbar />
-            <Outlet />
+            <div className="app-container">
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path="contact" element={<Contact />} />
+                </Routes>
+            </div>
         </>
     );
 };
