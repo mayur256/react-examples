ed207cc8846d8945e1ce9b05b17022a1de6a4141
Mayur Upadhayay <mayur123a@gmail.com>
2022-10-30 23:05:27 +0530

    [feat]: Adds composite tasklist component and stories

---
+++ taskbox/src/components/TaskList.stories.js
@@ -0,0 +1,45 @@
+import TaskList from './TaskList';
+import * as TaskStories from './Task.stories';
+
+export default {
+    title: 'TaskList',
+    component: TaskList,
+    decorators: [
+        story => <div style={{ padding: '3rem' }}>{story()}</div>
+    ],
+}
+
+const Template = (args) => <TaskList {...args} />;
+
+// Default view
+export const Default = Template.bind({});
+Default.args = {
+    tasks: [
+        { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
+        { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
+        { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
+        { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
+        { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
+        { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+    ],
+}
+
+export const WithPinnedTasks = Template.bind({});
+WithPinnedTasks.args = {
+    tasks: [
+        ...Default.args.tasks.slice(0, 5),
+        { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+    ],
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+    tasks: [],
+    loading: true,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+    tasks: [],
+    loading: false
+}
