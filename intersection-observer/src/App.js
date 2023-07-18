79751ac121a3f7894b290b1da4344d5abf693749
Mayur Upadhayay <mayur123a@gmail.com>
2023-07-18 23:40:01 +0530

    chore: Added image assets

--- intersection-observer/src/App.js
+++ intersection-observer/src/App.js
@@ -1,3 +1,17 @@
 export default function App() {
-    return <h1>Solar System</h1>;
+    return (
+        <>
+            <h1>Solar System</h1>
+            <ol>
+                <li>Mercury - Dark Gray</li>
+                <li>Venus - Pearly White</li>
+                <li>Earth - Blue</li>
+                <li>Mars - Brown-Red</li>
+                <li>Jupiter - Brown, Orange, Red, White</li>
+                <li>Saturn - hazy yellow-brown</li>
+                <li>Uranus - Blue-Green</li>
+                <li>Neptune - bright azure blue</li>
+            </ol>
+        </>
+    );
 };
