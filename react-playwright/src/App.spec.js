26e1096578f1c4b1156ac0b94d9cb8e6643831a4
Mayur Upadhayay <mayur123a@gmail.com>
2023-09-11 11:58:14 +0530

    chore: Bootstrapped a react app for selenium case study

--- react-playwright/src/App.spec.js
+++ react-playwright/src/App.spec.js
@@ -4,10 +4,14 @@ test("homepage has E2E Testing with Playwright", async ({ page }) => {
     // It will go to homepage of our React App
     await page.goto("http://127.0.0.1:5173/");
 
+    await page.screenshot({ path: "screenshot/before-input.png" });
+
     // Retreive the input
     const nameInput = await page.locator("id=nameInput");
     // Enter value to the field
     await nameInput.fill('Brix');
     // make assertion
     await expect(nameInput).toHaveValue('Brix');
+
+    await page.screenshot({ path: "screenshot/after-input.png" });
 });
