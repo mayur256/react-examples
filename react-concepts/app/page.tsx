b0d9bf8ed5d25d043e5f0df686aadbac5458c816
Mayur Upadhayay <mayur123a@gmail.com>
2023-10-06 23:57:43 +0530

    chore: Adds a basic layout for project

--- react-concepts/app/page.tsx
+++ react-concepts/app/page.tsx
@@ -1,8 +1,24 @@
 
 export default function Home() {
-  return (
-    <main>
-      App Details
-    </main>
-  )
+    return (
+        <main className="container mx-auto px-4">
+            <h1 className="text-5xl text-center">React Concepts</h1>
+
+            <div className="mt-4">
+                <p>
+                    This project is a humble attempt at exploring react concepts are are prevalent.
+                    I will be documenting all the concepts that I come across throughout my endeavour with proper examples.
+                    I also be discussing the throeritical aspects of the concepts along the way that you would wanna look out for.
+                </p>
+
+                <p>
+                    You can locate the specific concept through the list of links on the left side of the page.
+                </p>
+
+                <p className="text-2xl mt-2 text-center">
+                    So <span className="italic">without further ado</span>, let us commence our jouney!
+                </p>
+            </div>
+        </main>
+    )
 }
--- react-concepts/components/AppLinks/index.tsx
+++ react-concepts/components/AppLinks/index.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 export function AppLinks(): ReactElement {
     return (
         <ol>
+            <li><Link href="/">Home</Link></li>
             <li><Link href="/about">About</Link></li>
         </ol>
     )
