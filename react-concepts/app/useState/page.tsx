8675e172a1c42a83f2bbd8895326ff16b610bc92
Mayur Upadhayay <mayur123a@gmail.com>
2023-10-20 23:50:23 +0530

    chore: Enhanced useState example

--- react-concepts/app/useState/page.tsx
+++ react-concepts/app/useState/page.tsx
@@ -16,6 +16,21 @@ export default function UseState() {
             <div className='border-2 mt-2'>
                 <StateHook />
             </div>
+
+            <p className='mt-3 font-bold'>How do we update arrays and objects with setState?</p>
+            <p>It is recommended you always <em>replace the state value instead of mutating it directly as it does not allow React to render the component</em>.</p>
+            <p>Example</p>
+            <code className='bg-gray-100 p-1 rounded'>{`const [user, setUser] = useState({ name: 'John', age: 25 });`}</code>
+            <p className='mt-1'>Instead of this,</p>
+            <code className='bg-gray-100 p-1 rounded'>{`user.name = 'Alice' ;`}</code>
+            <p className='mt-1'>Do this</p>
+            <code className='bg-gray-100 p-1 rounded'>{`setUser({...user, name: 'Bob' });`}</code>
+
+            <p className='mt-3 font-bold'>How can use a state initializer?</p>
+            <p>React allows to pass a function to useState that provisions state initialisation</p>
+            <code className='bg-gray-100 p-1 rounded'>{`const [user, setUser] = useState(() => { return { name: storage.name } });`}</code>
+            <p>Here <em>storage</em> is basically a local data storage that can be used by react to initialise the component state.</p>
+
         </main>
     )
 }
