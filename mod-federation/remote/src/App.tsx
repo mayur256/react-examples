ed7451a989b13c02d445e1426fb8a06046180b0e
Mayur Upadhayay <mayur123a@gmail.com>
2023-12-26 22:40:40 +0530

    chore: Creates outline for remote and host app

---
+++ mod-federation/remote/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="remote-app-container">
+            <h2>From Remote Application</h2>
+            <button>Increase count</button>
+        </div>
+    )
+}
+
+export default App
