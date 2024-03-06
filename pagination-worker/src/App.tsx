eeb10c5e7550253a3b0b1e0eff13128b56cf2150
Mayur Upadhayay <mayur123a@gmail.com>
2024-03-07 00:03:44 +0530

    chore: adds ui for filter and sort

--- pagination-worker/src/App.tsx
+++ pagination-worker/src/App.tsx
@@ -1,11 +1,23 @@
-import { Box, } from "@mui/material";
+import { Box, TextField } from "@mui/material";
+import Select from 'react-select'
+
 import { CustomPaginate } from "./CustomPaginate";
 import { CustomTable } from "./CustomTable";
 
 export default function App() {
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+    ]
+
     return (
         <Box>
             <h1>Pagination Worker example</h1>
+            <Box sx={{ display: 'flex', marginBottom: 1, justifyContent: 'flex-end' }}>
+                <TextField label="Search" variant="standard" />
+                <Select placeholder="Sort" options={options} />
+            </Box>
             <CustomTable />
             <CustomPaginate />
         </Box>
