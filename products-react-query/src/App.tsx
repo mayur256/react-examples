c282bcb4286e7206319687869cce14800e3c551d
Mayur Upadhayay <mayur123a@gmail.com>
2024-04-14 19:13:25 +0530

    chore: Bootstrapped new demo project for react query

---
+++ products-react-query/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "flowbite-react";
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+            React Query example
+        </h1>
+        <Button>Click me</Button>
+        </div>
+    )
+}
+
+export default App
